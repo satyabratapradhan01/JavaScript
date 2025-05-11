64d6b1bd7f7c28d04f6498cef432b08a5d2eca49
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-12 00:43:35 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "satya@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+// accountId = 2 //not allowed
+
+accountEmail = "sonu@gmail.com";
+accountPassword = "112233";
+accountCity = "cuttack";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
