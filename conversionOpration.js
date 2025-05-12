54234201332f633752a62322c36ca6358c751ba7
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-13 00:33:47 +0530

    String

--- conversionOpration.js
+++ conversionOpration.js
@@ -23,5 +23,24 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber)
-console.log(stringNumber);
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+
+
+// opretion
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(true); 
+
+
+
+
+
---
+++ string.js
@@ -0,0 +1,23 @@
+const gameName = new String('satya..');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-4, 2);
+// console.log(anotherString);
+
+const newStringOne = "   sonu    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
