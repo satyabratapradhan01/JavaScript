e5861c402acd7059a4c938a10bf65b7703c3e37a
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-16 00:53:51 +0530

    truthy

---
+++ 04controlFlow/truthy.js
@@ -0,0 +1,16 @@
+const userEmail = "s@satya.ai"
+
+if (userEmail) {
+    console.log("Got User Email");
+} else {
+    console.log("Don't have user email");
+}
+
+/*
+falsy values
+false, 0, -0, BigInt 0n, "", null, nudefined, NaN
+
+truthy values
+"0", "false", " ", [] Empti Array, {} empti object, function(){}
+*/ 
+
