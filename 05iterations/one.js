bd3511508e6445806ada19fce39e21f14ed67e7a
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-16 16:13:25 +0530

    forloop

---
+++ 05iterations/one.js
@@ -0,0 +1,24 @@
+// for 
+
+// for (let i = 0; i <= 10; i++) {
+//     let element = i;
+//     if(element == 5){
+//         console.log("5 is best number");
+        
+//     }
+//     console.log(i);
+// }
+
+// for(let i=0; i<=10; i++){
+//     console.log(`outer loop value ${i}`);
+//     for(let j=0; j<=10; j++){
+//         console.log(`Inner loop value ${j} and outer loop value ${i}`);
+//     }
+// }
+
+
+// let myArray = ["flash", "batman", "superman"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
