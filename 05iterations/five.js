5291128297cc9dd4b965acd5ba9b1a613b427449
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-17 10:30:27 +0530

    maps

---
+++ 05iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ['js', 'rb', 'py', 'java', 'cpp', 'python'];
+
+// coding.forEach( function (val){
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+function printMe(item){
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr)
+// })
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileNaame: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileNaame: "java"
+    },
+    {
+        languageName: "python",
+        languageFileNaame: "py"
+    },
+    
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageFileNaame);
+    
+})
