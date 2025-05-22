4e8659dbf372ecc2ff054fb28b0a144d19ae4eb4
satyabratapradhan <satyabratapradhann@gmail.com>
2025-05-22 16:10:05 +0530

    opps2

--- 10classandoop/oop.js
+++ 10classandoop/oop.js
@@ -16,3 +16,15 @@ const user = {
 // console.log(user.getUserDetails());
 // console.log(this);
 
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
+}
+
+const userOne = new User('satya', 12, true);
+const userTwo = new User("hi satya", 11, false);
+
+console.log(userOne);
